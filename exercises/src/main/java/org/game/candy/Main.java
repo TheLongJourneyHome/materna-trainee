63ccd5f52952bc50d99801b91fb2f44840b40088
dunguyen <duc.nugyen@materna.de>
2023-01-13 09:21:08 +0100

    Edit Radio UML and first UML version of CandyLord

---
+++ exercises/src/main/java/org/game/candy/Main.java
@@ -0,0 +1,31 @@
+package org.game.candy;
+
+public class Main {
+    // Testing and Code Coverage
+/*
+    IntelliJ IDEA is an integrated development environment (IDE) for Java, Kotlin, and other programming languages.
+    One of the features of the IDE is the ability to customize the color scheme,
+    which allows you to change the colors used for various elements of the user interface, such as the background, text, and syntax highlighting.
+
+    In addition to the color scheme, IntelliJ IDEA also provides a line coverage feature,
+    which allows you to see how much of your code is covered by tests.
+    The line coverage feature uses different colors to indicate the coverage status of each line of code:
+
+        Full coverage:
+            This color is used to indicate lines of code that have been executed during a test and
+            that have passed.
+            The default color for full coverage is green.
+        Partial coverage:
+            This color is used to indicate lines of code that have been executed during a test
+            but that have not been fully covered.
+            The default color for partial coverage is yellow.
+        Uncovered:
+            This color is used to indicate lines of code that have not been executed during a test.
+            The default color for uncovered lines is red.
+
+    You can configure the colors for the line coverage by go to settings->Editor->Code coverage and
+    you can find the options to change the colors according to your preferences.*/
+    public static void main(String[] args) {
+
+    }
+}
